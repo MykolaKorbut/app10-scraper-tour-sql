83c7ee745aa3c1e4a2c1a0db6f8eb74a32cc5bd6
mikolakorbut <nikolay.korbut@gmail.com>
2024-09-15 10:42:41 +0300

    initial commit. Scrape the webpage

---
+++ main.py
@@ -0,0 +1,20 @@
+from http.client import responses
+
+import requests
+import selectorlib
+
+URL = 'http://programmer100.pythonanywhere.com/tours/'
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 '
+                  'Safari/537.36'}
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    responses = requests.get(url=url, headers=HEADERS)
+    source = responses.text
+    return source
+
+if __name__ == "__main__":
+    print(scrape(URL))
