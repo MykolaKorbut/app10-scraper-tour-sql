3b1e072e986ccb718a83a914d3d92fc90e5a6072
mikolakorbut <nikolay.korbut@gmail.com>
2024-09-17 10:50:54 +0300

    Storing the Extracted Data in SQL database

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
 import selectorlib
+import smtplib
+import ssl
+import time
+import sqlite3
+
+"INSERT INTO events VALUES ('Tigers', 'Tiger city', '2088.10.14')"
+"SELECT * FROM events WHERE date='2088.10.15'"
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
-
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 '
                   'Safari/537.36'}
+
+connection = sqlite3.connect("data.db")
+
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -20,25 +28,52 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "nikolay.korbut@gmail.com"
+    password = "juff wljr qdeq anbc"
+
+    receiver = "nikolay.korbut@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Emil was sent!")
 
 
 def store(extracted):
-    with open("data.txt", 'a') as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES (?, ?, ?)", row)
+    connection.commit()
+    print(row)
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND "
+                   "date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        if extracted != "No upcoming tours":
+            row = read(extracted)
+            if not row:
+                store(extracted)
+                send_email(message="Hey!")
+        time.sleep(5)
